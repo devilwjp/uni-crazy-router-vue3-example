177f7191dfbfbd70da6b4eb4510806cb67ceeea7
devilwjp <9700616@qq.com>
2022-02-12 20:21:59 +0800

    feat: 添加对h5 build环境的特殊处理使用说明

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import {
 } from "vue";
 import App from "./App.vue";
 import { setupRouter } from './router'
+
+/**
+ * 对于H5 production环境做特殊处理
+ * 让vite打包时在uni对象里强制标记5个方法，否则vite会用内部函数指针把代码中的字面量全部覆盖
+ * 必须字面量描述，不能动态描述
+ * 以下使用条件编译
+ */
+// #ifdef H5 && production
+const navigateTo = uni.navigateTo
+const redirectTo = uni.redirectTo
+const reLaunch = uni.reLaunch
+const switchTab = uni.switchTab
+const navigateBack = uni.navigateBack
+uni.navigateTo = navigateTo
+uni.redirectTo = redirectTo
+uni.reLaunch = reLaunch
+uni.switchTab = switchTab
+uni.navigateBack = navigateBack
+// #ifndef
+
 export function createApp() {
 	const app = createSSRApp(App);
 	setupRouter(app)
