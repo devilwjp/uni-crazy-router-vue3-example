8c1b681dce963c4c1e8ec09a49ea604108c5cbc8
devilwjp <9700616@qq.com>
2022-03-09 01:45:20 +0800

    🚀🪂👽

---
+++ src/pages/index/tabbar1.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "tabbar1"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index/tabbar2.vue
@@ -0,0 +1,13 @@
+<template>
+  <view>tabbar1</view>
+</template>
+
+<script>
+export default {
+  name: "tabbar1"
+}
+</script>
+
+<style scoped>
+
+</style>
